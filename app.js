b2d8cdd74ab8680aa2284eaf3896aa6f66e82200
Notonyus_risk <antoniojoserudassanchez@gmail.com>
2024-02-20 21:28:18 -0500

    feat::app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+let btn_modal1 = document.getElementById("open-modal1")
+let btn_modal2 = document.getElementById("open-modal2")
+let btn_close1 = document.getElementById("close-modal1")
+let btn_close2 = document.getElementById("close-modal2")
+
+let modal1 = document.getElementById("modal1")
+let modal2 = document.getElementById("modal2")
+
+btn_modal1.onclick = function() {
+    modal1.style.display = "block"
+}
+
+btn_modal2.onclick = function() {
+    modal2.style.display = "block"
+}
+
+btn_close1.onclick = function() {
+    modal1.style.display = "none"
+}
+
+btn_close2.onclick = function() {
+    modal2.style.display = "none"
+}
