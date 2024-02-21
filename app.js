1ca52907c123fc5d6d83508c36f977b0baa23d94
Notonyus_risk <antoniojoserudassanchez@gmail.com>
2024-02-20 21:51:03 -0500

    feat::styles.css

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ let btn_close2 = document.getElementById("close-modal2")
 let modal1 = document.getElementById("modal1")
 let modal2 = document.getElementById("modal2")
 
+//abrir modals
 btn_modal1.onclick = function() {
     modal1.style.display = "block"
 }
@@ -14,6 +15,7 @@ btn_modal2.onclick = function() {
     modal2.style.display = "block"
 }
 
+//cerrar modals
 btn_close1.onclick = function() {
     modal1.style.display = "none"
 }
