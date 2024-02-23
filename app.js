84aec00f55b5fe166f46a7bc2a933d70d281a687
catalinafs <catalinaforerosuarez@gmail.com>
2024-02-23 17:46:09 -0500

    feedback

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
-let btn_modal1 = document.getElementById("open-modal1")
-let btn_modal2 = document.getElementById("open-modal2")
-let btn_close1 = document.getElementById("close-modal1")
-let btn_close2 = document.getElementById("close-modal2")
+let btn_modal1 = document.getElementById("open-modal1");
+let btn_modal2 = document.getElementById("open-modal2");
+let btn_close1 = document.getElementById("close-modal1");
+let btn_close2 = document.getElementById("close-modal2");
 
-let modal1 = document.getElementById("modal1")
-let modal2 = document.getElementById("modal2")
+let modal1 = document.getElementById("modal1");
+let modal2 = document.getElementById("modal2");
 
 //abrir modals
-btn_modal1.onclick = open_modal1
+btn_modal1.onclick = open_modal1;
 
 function open_modal1() {
-    modal1.style.display = "block"
+    modal1.style.display = "block";
 }
 
-btn_modal2.onclick = open_modal2
+btn_modal2.onclick = open_modal2;
 
 function open_modal2() {
-    modal2.style.display = "block"
+    modal2.style.display = "block";
 }
 
 //cerrar modals
-btn_close1.onclick = function() {
-    modal1.style.display = "none"
+btn_close1.onclick = function () {
+    modal1.style.display = "none";
 }
 
-btn_close2.onclick = function() {
-    modal2.style.display = "none"
+btn_close2.onclick = () => {
+    modal2.style.display = "none";
 }
